d0a4926da79c5a1afda2027ae8cfc6b2f2670633
5Prabesh <pbakhrel5@gmail.com>
2024-06-06 17:27:50 +0545

    image added and SplashScreen page completed

--- src/AuthScreens/SplashScreen.js
+++ src/AuthScreens/SplashScreen.js
@@ -1,12 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
-const SplashScreen = () => {
-  return (
-    <View>
-      <Text>SplashScreen</Text>
-    </View>
-  )
-}
+const SplashScreen = ({ navigation }) => {
+
+    const [isvisible, setIsVisible] = useState(true);
+
+    useEffect(() => {
+        const Timer = setTimeout(() => {
+            setIsVisible(false);
+            navigation.navigate('LoginScreen');
+        }, 2000);
+
+        return () => clearTimeout(Timer);
+    }, [navigation])
+
+    if (!isvisible) {
+        return null;
+    }
+
+    return (
+
+        <View style={{
+            flex: 1
+        }}>
+            <View style={{
+                marginTop: '30%',
+                alignContent: 'center',
+                alignItems: 'center'
+            }}>
+                <Image source={require('../assets/Logo/wolf.png')} style={{
+                    height: '80%',
+                    width: '70%'
+                }} />
+            </View>
+            <Text style={{
+                color: 'black',
+                fontWeight: 'bold',
+                fontSize: 40,
+                textAlign: 'center',
+                fontFamily: 'cursive'
+            }}>
+                Welcome
+            </Text>
+        </View>
+    );
+};
 
 export default SplashScreen
