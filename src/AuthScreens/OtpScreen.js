f72cbb570b541f99e3c320443a7d34e1ceb49968
5Prabesh <pbakhrel5@gmail.com>
2024-06-09 22:25:43 +0545

    otpscreen added

---
+++ src/AuthScreens/OtpScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OtpScreen = () => {
+  return (
+    <View>
+      <Text>OtpScreen</Text>
+    </View>
+  )
+}
+
+export default OtpScreen
